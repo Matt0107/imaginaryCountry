46c783d5c4958ce91022471d322b3bef6ddfe6f9
Nevin Alisheva <nevin.alisheva@gmail.com>
2022-09-30 00:48:00 +0200

    almost done

--- resources/js/index.js
+++
@@ -1,8 +0,0 @@
-function navFunction() {
-    const x = document.querySelector(".dropdown-content");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block";
-    }
-}
