84790f5bedf65d80671e9057006a9a029aada963
stefan <stefan@stefan.com>
2022-10-04 13:19:03 +0200

    minor changes

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,8 +1,8 @@
-const place = document.querySelector('.popular-place')
-const description = document.querySelector('.placeDescription');
-const picture = document.querySelector('.place')
+// const place = document.getElementsByClassName('popular-place')
+// const description = document.querySelector('.placeDescription');
+// const picture = document.querySelector('.place')
 
-picture.addEventListener('click', function () {
-    description.classList.toggle('activeDescription')
-}
-)
+// picture.addEventListener('click', function () {
+//     description.classList.toggle('activeDescription')
+// }
+// )
